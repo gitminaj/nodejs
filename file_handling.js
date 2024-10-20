3c64063d906a5760e42cef4a0d8ac9b415b7edcf
Minaj <minajinamdar9@gmail.com>
2024-10-20 23:14:54 +0530

    basics of nodejs file handling and creasting server

---
+++ file_handling.js
@@ -0,0 +1,19 @@
+const fs = require("fs")
+
+
+// fs.writeFile("./text.txt", "hello wrireFile",()=>{});
+
+// fs.appendFile("./text.txt" , " appended daata", ()=>{});
+
+// fs.readFile("./text.txt", "utf-8", (error, result) =>{
+//     if(error){
+//         console.log(error);
+//     }
+//     else{
+//      console.log(result)
+//     }
+// })
+
+// fs.cp("./text.txt", "./copy_text.txt", ()=>{}) // copy files
+
+// fs.unlink("./copy_text.txt", ()=>{})  // delete files
